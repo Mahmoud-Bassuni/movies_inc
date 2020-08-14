bbcc96e7f8caf9881fea24bc84200ac7338072ad
mahmoud bassuni <mahmoudbasuni90@gmail.com>
2020-08-14 11:56:18 +0200

    remove podfile.lock

--- movies_inc/ViewController.swift
+++ movies_inc/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // remove podfile.lock
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
